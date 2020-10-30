5142b1544d3951d78eff2d5fb78dbd2e61af7b74
MohammedAl-rowad <rowadwo@gmail.com>
2020-10-30 14:20:41 +0200

    style the tasks

--- js/index.js
+++ js/index.js
@@ -11,7 +11,23 @@ const getInputText = () => {
 const insertTaskIntoPage = (text) => {
   document
     .getElementById('tasks-section')
-    .insertAdjacentHTML('beforeend', `<h3>${text}</h3>`)
+    .insertAdjacentHTML('beforeend', `
+    <div class="card rounded-pill mt-2 text-left">
+        <div class="card-body">
+            <h5 class="card-title">${text}</h5>
+            <button type="button" class="btn btn-success rounded-pill">
+                <span class="material-icons">
+                    done_outline
+                </span>
+            </button>
+            <button type="button" class="btn btn-danger rounded-pill">
+                <span class="material-icons">
+                    delete_forever
+                </span>
+            </button>
+        </div>
+    </div>
+    `)
 }
 
 const emptyInputText = () => {

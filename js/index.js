78e0f0cd35996a5966c8535f693683a902fadb60
MohammedAl-rowad <rowadwo@gmail.com>
2020-10-30 15:55:24 +0200

    Handle delete and update

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 document.getElementById('create-task-id').addEventListener('click', () => {
   const txt = getInputText()
-  insertTaskIntoPage(txt)
   saveTask(txt)
   emptyInputText()
 })
@@ -9,19 +8,21 @@ const getInputText = () => {
   return document.getElementById('task-text').value
 }
 
-const insertTaskIntoPage = (text) => {
+const insertTaskIntoPage = (task) => {
   document.getElementById('tasks-section').insertAdjacentHTML(
     'beforeend',
     `
-    <div class="card rounded-pill mt-2 text-left">
-        <div class="card-body">
-            <h5 class="card-title">${text}</h5>
-            <button type="button" class="btn btn-success rounded-pill">
+    <div class="card rounded-pill mt-2 text-left ${
+      task.isFinished ? 'shadow-lg bg-dark text-white' : ''
+    }">
+        <div class="card-body" data-task-object='${JSON.stringify(task)}'>
+            <h5 class="card-title">${task.txt}</h5>
+            <button type="button" class="btn btn-success rounded-pill success">
                 <span class="material-icons">
                     done_outline
                 </span>
             </button>
-            <button type="button" class="btn btn-danger rounded-pill">
+            <button type="button" class="btn btn-danger rounded-pill delete">
                 <span class="material-icons">
                     delete_forever
                 </span>
@@ -32,14 +33,6 @@ const insertTaskIntoPage = (text) => {
   )
 }
 
-const tasks = JSON.parse(localStorage.getItem('tasks'))
-
-if (tasks) {
-  tasks.forEach((task) => {
-    insertTaskIntoPage(task)
-  })
-}
-
 const emptyInputText = () => {
   document.getElementById('task-text').value = ''
 }
@@ -47,9 +40,51 @@ const emptyInputText = () => {
 const saveTask = (txt) => {
   const tasks = JSON.parse(localStorage.getItem('tasks'))
   if (tasks) {
-    tasks.push(txt)
+    tasks.push({ txt, id: shortid.generate() })
     localStorage.setItem('tasks', JSON.stringify(tasks))
   } else {
-    localStorage.setItem('tasks', JSON.stringify([txt]))
+    const id = shortid.generate()
+    localStorage.setItem('tasks', JSON.stringify([{ txt, id }]))
+  }
+  renderData()
+}
+
+const renderData = () => {
+  document.getElementById('tasks-section').innerHTML = ''
+  const tasks = JSON.parse(localStorage.getItem('tasks'))
+
+  if (tasks) {
+    tasks.forEach((task) => {
+      insertTaskIntoPage(task)
+    })
+  }
+  document.querySelectorAll('.success').forEach((node) => {
+    node.addEventListener('click', handleDeleteAndUpdate())
+  })
+
+  document.querySelectorAll('.delete').forEach((node) => {
+    node.addEventListener('click', handleDeleteAndUpdate('filter'))
+  })
+}
+
+const handleDeleteAndUpdate = function (type) {
+  return function () {
+    const data = JSON.parse(this.parentElement.dataset['taskObject'])
+    const tasks = JSON.parse(localStorage.getItem('tasks'))
+    const newData =
+      type === 'filter'
+        ? tasks.filter((el) => {
+            return data.id !== el.id
+          })
+        : tasks.map((el) => {
+            if (data.id === el.id) {
+              el.isFinished = true
+            }
+            return el
+          })
+    localStorage.setItem('tasks', JSON.stringify(newData))
+    renderData()
   }
 }
+
+renderData()
